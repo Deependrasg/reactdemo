775463ba9f2786f96bd0e0c6e99acb98c1c729a8
Deependrasg <deependrasg@gmail.com>
2018-01-19 17:59:25 +0530

    Delete Unused file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let object={
+	fname:'Client3',
+	lname:'Client5'
+}
+
+class Deep extends React.Component{
+	render(){
+		return (<h1> hi {object.fname},{object.lname}</h1>);
+	}
+}
+
+ let name= 'Deeepndra singh'
+class Deep1 extends React.Component{
+	render(){
+		return (<h1>Heeeld {name}</h1>);
+	}
+}
+
+
+class Deepsingh extends React.Component{
+	render(){
+		return (<h4> <Deep/> <Deep1/> </h4>);
+	}
+}
+
+
+ReactDOM.render(<Deepsingh/>, document.getElementById('root'));
 registerServiceWorker();
